a97a2e0198071665a7c0fe39aa73d25430ee7614
Junior <nog.junior84@gmail.com>
2016-05-03 23:06:15 -0300

    Model Music e inicio do teste band_spec relacionamento

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504012012) do
+ActiveRecord::Schema.define(version: 20160504020353) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160504012012) do
     t.string   "site"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "musics", force: :cascade do |t|
+    t.string   "title"
+    t.string   "name_author"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Band, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:musical_genre) }
   it { is_expected.to validate_presence_of(:site) }
+
+  it { is_expected.to has_many(:musics) }
 end
