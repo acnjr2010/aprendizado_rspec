a1bc59367d6b2e1044d40462695d23779d52270c
Junior <nog.junior84@gmail.com>
2016-05-03 22:56:38 -0300

    Testando Validacoes

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Band, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:musical_genre) }
+  it { is_expected.to validate_presence_of(:site) }
 end
